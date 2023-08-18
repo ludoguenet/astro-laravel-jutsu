98d6bb704943b23431ece4839371b3b3d08c0b8f
Ludovic Guénet <lg@slym.io>
2023-08-18 23:13:41 +0200

    fix: site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://laraveljutsu.net',
   integrations: [mdx(), sitemap(), tailwind()],
   experimental: {
     viewTransitions: true

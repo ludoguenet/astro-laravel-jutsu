8a17d4f01b9addcaf471d4c79328a49fe22fc996
Ludovic Guénet <lg@slym.io>
2023-09-27 21:28:01 +0200

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,4 +16,10 @@ export default defineConfig({
       wrap: false,
     },
   },
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });

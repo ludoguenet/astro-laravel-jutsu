27836c721b4aea46374afad655e5c869c5de49fb
ludoguenet <ludovicguenet@gmx.com>
2024-06-27 17:48:43 +0200

    feat: back to top

---
+++ src/scripts/back-to-top.ts
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (backToTopButton) {
+            if (window.scrollY > 300) {
+                backToTopButton.classList.add('show');
+            } else {
+                backToTopButton.classList.remove('show');
+            }
+        }
+    });
+
+    backToTopButton?.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});

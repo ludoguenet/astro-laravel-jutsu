d1f6da5f55ab5e726e8706b10460a93a4df51df5
ludoguenet <ludovicguenet@gmx.com>
2024-11-24 01:04:12 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import syntaxTheme from './syntax-theme.json';
+import { defineConfig, passthroughImageService } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import syntaxTheme from "./syntax-theme.json";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://laraveljutsu.net',
+  site: "https://laraveljutsu.net",
   image: {
     service: passthroughImageService(),
   },
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: syntaxTheme,
+      theme: "material-theme-palenight",
       langs: [],
       wrap: false,
     },

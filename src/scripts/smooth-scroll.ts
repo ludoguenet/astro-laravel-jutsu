3749d6e0da326e2bdcd68bf9bdd574471378e64c
slym-lg <ludo@epekta.com>
2024-12-20 11:27:11 +0100

    fix

--- src/scripts/smooth-scroll.ts
+++ src/scripts/smooth-scroll.ts
@@ -1,27 +1,32 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    function smoothScroll(targetElement: Element) {
-        targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
-    }
-
-    function handleClick(event: Event) {
-        event.preventDefault();
-
-        const href = (event.target as HTMLAnchorElement).getAttribute('href');
-
-        if (href && href.startsWith('#')) {
-            const targetId = href.substring(1);
-            const elementFromId: Element | null = document.querySelector(`a[name="${targetId}"]`);
-
-            if (elementFromId) {
-                smoothScroll(elementFromId);
-            }
-        }
-    }
-
-    document.querySelectorAll('a[href^="#"]').forEach(link => {
-        link.addEventListener('click', handleClick);
+window.addEventListener("DOMContentLoaded", (event) => {
+  function smoothScroll(targetElement: Element) {
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
+  }
+
+  function handleClick(event: Event) {
+    event.preventDefault();
+
+    const href = (event.currentTarget as HTMLAnchorElement).getAttribute(
+      "href"
+    );
+
+    if (href && href.startsWith("#")) {
+      const targetId = href.substring(1);
+
+      const elementFromId: Element | null =
+        document.querySelector(`a[name="${targetId}"]`) ||
+        document.getElementById(targetId);
+
+      if (elementFromId) {
+        smoothScroll(elementFromId);
+      }
+    }
+  }
+
+  document.querySelectorAll('a[href^="#"]').forEach((link) => {
+    link.addEventListener("click", handleClick);
+  });
 });
